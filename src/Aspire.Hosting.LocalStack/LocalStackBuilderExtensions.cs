d88a700dda96cb39d3d9d76db243ac143b799072
Ross King <ross.king@altavec.com>
2025-02-04 08:47:52 +1100

    ⚡️ use supplied constant

--- src/Aspire.Hosting.LocalStack/LocalStackBuilderExtensions.cs
+++ src/Aspire.Hosting.LocalStack/LocalStackBuilderExtensions.cs
@@ -70,7 +70,7 @@ public static class LocalStackBuilderExtensions
         _ = builder.WithEnvironment((context) =>
         {
             // disable the AWS EC2 credentials lookup, as we are local
-            context.EnvironmentVariables["AWS_EC2_METADATA_DISABLED"] = bool.TrueString;
+            context.EnvironmentVariables[Amazon.Util.EC2InstanceMetadata.AWS_EC2_METADATA_DISABLED] = bool.TrueString;
 
             context.EnvironmentVariables[$"{LocalStackConfigSection}__UseLocalStack"] = bool.TrueString;
 
